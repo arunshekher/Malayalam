fb9df24024f4c232b6d713403ee39cc7109fb330
Arun S. Sekher <arunshekher@users.noreply.github.com>
2017-05-25 22:27:07 +0400

    Add file e107_languages/Malayalam/lan_contact.php

---
+++ e107_languages/Malayalam/lan_contact.php
@@ -0,0 +1,35 @@
+<?php
+/*
++---------------------------------------------------------------+
+|        e107 website content management system Malayalam Language File
+|        Released under the terms and conditions of the
+|        GNU General Public License (http://gnu.org).
+|        Last Modified: 2017/05/25 18:21:09
+|
+|        $Author: Arun $
++---------------------------------------------------------------+
+*/
+define("LANCONTACT_00", "ഞങ്ങളോട് സന്പര്‍ക്കപ്പെടുക");
+define("LANCONTACT_01", "സന്പര്‍ക്ക വിവരങ്ങൾ");
+define("LANCONTACT_02", "സന്പര്‍ക്ക ഫോം");
+define("LANCONTACT_03", "നിങ്ങളുടെ പേര് നൽകുക");
+define("LANCONTACT_04", "നിങ്ങളുടെ ഇമെയിൽ വിലാസം നൽകുക");
+define("LANCONTACT_05", "സന്ദേശ വിഷയം");
+define("LANCONTACT_06", "സന്ദേശം");
+define("LANCONTACT_07", "നിങ്ങളുടെ സ്വന്തം വിലാസത്തിലേക്ക് ഈ സന്ദേശത്തിന്റെ ഒരു പകർപ്പ് ഇമെയിൽ ചെയ്യുക");
+define("LANCONTACT_08", "അയയ്ക്കുക");
+define("LANCONTACT_09", "നിങ്ങളുടെ സന്ദേശം അയച്ചു.");
+define("LANCONTACT_10", "നിങ്ങളുടെ സന്ദേശം അയക്കുന്നതിൽ ഒരു പ്രശ്നമുണ്ടായി.");
+define("LANCONTACT_11", "ദയവായി നിങ്ങളുടെ ഇമെയിൽ വിലാസം പരിശോധിച്ച് സന്പര്‍ക്ക ഫോം വീണ്ടും സമർപ്പിക്കുക.");
+define("LANCONTACT_12", "നിങ്ങളുടെ സന്ദേശം വളരെ ചെറുതാണ്.");
+define("LANCONTACT_13", "ഒരു വിഷയം ചേർക്കുക.");
+define("LANCONTACT_14", "സന്ദേശം അയയ്ക്കുക");
+define("LANCONTACT_15", "തെറ്റായ കോഡ് നൽകി");
+define("LANCONTACT_16", "ഈ ഫോം ഉപയോഗിക്കുന്നതിന് നിങ്ങൾ [രജിസ്റ്റർ] ചെയ്യുകയും സൈൻ ഇൻ ചെയ്യപ്പെടുകയും വേണം.");
+define("LANCONTACT_17", "ദയവായി നിങ്ങളുടെ പേര് നൽകുക.");
+define("LANCONTACT_18", "ദയവായി നിങ്ങളുടെ ഇ-മെയിൽ വിലാസം രേഖപ്പെടുത്തുക.");
+define("LANCONTACT_19", "ദയവായി നിങ്ങളുടെ ഇ-മെയിലിനുള്ള വിഷയം നൽകുക.");
+define("LANCONTACT_20", "ദയവായി ഞങ്ങൾക്കുള്ള നിങ്ങളുടെ സന്ദേശം ചേർക്കുക.");
+
+
+?>
