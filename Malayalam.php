0bee996db495e138d29d6d4d7e734231355a53a2
Arun S. Sekher <arunshekher@users.noreply.github.com>
2017-05-25 21:05:58 +0400

    Initial commit - e107 Malayalam language pack
    Add files Malayalam.xml and Malayalam.php

---
+++ Malayalam.php
@@ -0,0 +1,132 @@
+<?php
+/*
++---------------------------------------------------------------+
+|        e107 website content management system Malayalam Language File
+|        Released under the terms and conditions of the
+|        GNU General Public License (http://gnu.org).
+|        Last Modified: 2017/05/25 14:44:34
+|
+|        $Author: Arun $
++---------------------------------------------------------------+
+*/
+setlocale(LC_ALL,'ml_IN.UTF-8', 'ml.utf8', 'ml_ML.utf8', 'ml.UTF-8');
+define("CORE_LC", "ml");
+define("CORE_LC2", "ml");
+define("CORE_LAN1", "പിശക്‌: തീം കണ്ടെത്താനാവുന്നില്ല.\\n\\nഉപയോഗിച്ചു കൊണ്ടിരുന്ന തീം നിങ്ങളുടെ പ്രഥമഗണനയിൽ (അഡ്മിൻ ഏരിയ) നിന്ന് മാറ്റുക അല്ലെങ്കിൽ നിലവിലുള്ള തീമിന്റെ നഷ്ടപ്പെട്ട ഫയലുകൾ സെർവറിലേക്കു അപ്‌ലോഡ് ചെയ്യുക.");
+define("CORE_LAN4", "ദയവായി install.php നിങ്ങളുടെ സെർവറിൽനിന്നു ഡിലീറ്റ് ചെയ്യുക");
+define("CORE_LAN5", "ഇല്ലാത്ത പക്ഷം നിങ്ങളുടെ വെബ്സൈറ്റിന്റെ സുരക്ഷക്കു അത് ഭീഷണിയായി മാറാം.");
+define("CORE_LAN6", "ഈ സൈറ്റിന്റെ ഫ്ലഡ് സംരക്ഷണം ആക്ടിവേറ്റ് ചെയ്തു, നിങ്ങൾ ഇത്ര വേഗത്തിൽ പേജുകൾ ആവശ്യപ്പെട്ടു കൊണ്ടിരുന്നാൽ  നിങ്ങൾ നിരോധിക്കപ്പെടുമെന്ന് മുന്നറിയിപ്പ് നൽകുന്നു.");
+define("CORE_LAN7", "മുൻപത്തെ സ്വയം നടത്തിയ ബാക്കപ്പിൽ നിന്നും പ്രഥമഗണനകൾ പുനഃസ്ഥാപിക്കാൻ കോർ ശ്രമിക്കുന്നു.");
+define("CORE_LAN8", "കോർ പ്രഥമഗണന പിശക്");
+define("CORE_LAN9", "സ്വയം നടത്തിയ ബാക്കപ്പിൽ നിന്ന് പ്രഥമഗണനകൾ പുനഃസ്ഥാപിക്കാൻ കോറിനു കഴിഞ്ഞില്ല. നടത്തിപ്പ്‌ നിർത്തി വച്ചു.");
+define("CORE_LAN10", "ദൂഷിതമായ കുക്കി കണ്ടെത്തി - ആയതിനാൽ ലോഗ് ഔട്ട് ചെയ്തു.");
+define("CORE_LAN11", "റെൻഡർ സമയം:");
+define("CORE_LAN12", "സെക്കന്റ് (");
+define("CORE_LAN13", "% അതിൽ ക്യുയറികൾക്കായി");
+define("CORE_LAN14", "%2.3f സി.പി.യു സെക്കന്റ് (% 2.2f %% ലോഡ്,% 2.3f സ്റ്റാർട്ടപ്പ്). ക്ലോക്ക്:");
+define("CORE_LAN15", "ഡി.ബി ക്യുയറികൾ:");
+define("CORE_LAN16", "മെമ്മറി:");
+define("CORE_LAN17", "[ചിത്രം അപ്രാപ്തമാക്കി]");
+define("CORE_LAN18", "ചിത്രം:");
+define("CORE_LAN_B", "ബി");
+define("CORE_LAN_KB", "കെ.ബി");
+define("CORE_LAN_MB", "എം.ബി");
+define("CORE_LAN_GB", "ജി.ബി");
+define("CORE_LAN_TB", "ടി.ബി");
+define("EMESSLAN_TITLE_INFO", "സിസ്റ്റം വിവരങ്ങൾ");
+define("EMESSLAN_TITLE_ERROR", "പിശക്‌");
+define("EMESSLAN_TITLE_SUCCESS", "വിജയം");
+define("EMESSLAN_TITLE_WARNING", "മുന്നറിയിപ്പ്");
+define("EMESSLAN_TITLE_DEBUG", "സിസ്റ്റം ഡീബഗ്");
+define("LAN_NO_PERMISSIONS", "ഈ പേജ് കാണുന്നതിന് നിങ്ങൾക്ക് അനുമതിയില്ല.");
+define("LAN_EDIT", "തിരുത്തുക");
+define("LAN_DELETE", "നീക്കം ചെയ്യുക");
+define("LAN_DEFAULT", "ഡീഫോൾട്");
+define("LAN_MORE", "കൂടുതൽ..");
+define("LAN_LESS", "..കുറച്ച്");
+define("LAN_READ_MORE", "കൂടുതൽ വായിക്കുക..");
+define("LAN_GOPAGE", "പേജിലേക്ക് പോകുക");
+define("LAN_GOTOPAGEX", "പേജിലേക്ക് പോകുക [x]");
+define("LAN_GO", "പോകുക");
+define("LAN_SUBMIT", "സമർപ്പിക്കുക");
+define("LAN_NONE", "ഒന്നുമില്ല");
+define("LAN_WARNING", "മുന്നറിയിപ്പ്");
+define("LAN_ERROR", "പിശക്");
+define("LAN_ANONYMOUS", "അജ്ഞാത");
+define("LAN_EMAIL_SUBS", "-ഇമെയിൽ-");
+define("LAN_ACTIVE", "സജീവമാണ്");
+define("LAN_YES", "അതെ");
+define("LAN_NO", "ഇല്ല");
+define("LAN_OK", "ശരി");
+define("LAN_ACTIONS", "പ്രവൃത്തികൾ");
+define("LAN_THANK_YOU", "നന്ദി");
+define("LAN_CONTINUE", "തുടരുക");
+define("LAN_ENTER", "പ്രവേശിക്കുക");
+define("LAN_ENTER_CODE", "കോഡ് നൽകുക");
+define("LAN_INVALID_CODE", "തെറ്റായ കോഡ് നൽകിയിരിക്കുന്നു.");
+define("LAN_SEARCH", "തിരയുക");
+define("LAN_VIEW", "കാണുക");
+define("LAN_CLICK_TO_VIEW", "കാണുന്നതിന് ക്ലിക്കുചെയ്യുക");
+define("LAN_SORT", "തരം തിരിക്കുക");
+define("LAN_ORDER_BY", "ക്രമപ്പെടുത്തുക ഇത്തരത്തിൽ");
+define("LAN_ASCENDING", "ആരോഹണം");
+define("LAN_DESCENDING", "അവരോഹണം");
+define("LAN_SHARE", "പങ്കിടുക");
+define("LAN_BACK", "തിരികെ");
+define("LAN_NAME", "പേര്");
+define("LAN_DESCRIPTION", "വിവരണം");
+define("LAN_CANCEL", "റദ്ദാക്കുക");
+define("LAN_DATE", "തീയതി");
+define("LAN_DATE_POSTED", "പോസ്റ്റുചെയ്ത തീയതി");
+define("LAN_POSTED_BY", "പോസ്റ്റ് ചെയ്തത്");
+define("LAN_JSCONFIRM", "നിങ്ങൾക്ക് ഉറപ്പാണോ?");
+define("LAN_IP", "ഐ.പി");
+define("LAN_IP_ADDRESS", "ഐ.പി വിലാസം");
+define("LAN_AUTHOR", "രചയിതാവ്");
+define("LAN_CATEGORY", "വിഭാഗം");
+define("LAN_GUEST", "അതിഥി");
+define("LAN_NEXT", "അടുത്തത്");
+define("LAN_PREVIOUS", "മുന്‍പുള്ള");
+define("LAN_LOGIN", "ലോഗിൻ");
+define("LAN_LOGOUT", "പുറത്തുകടക്കുക");
+define("LAN_SETTINGS", "ക്രമീകരണങ്ങൾ");
+define("LAN_PASSWORD", "അടയാളവാക്ക്");
+define("LAN_INCORRECT_PASSWORD", "തെറ്റായ അടയാളവാക്ക്");
+define("LAN_TYPE", "തരം");
+define("LAN_SCREENSHOT", "സ്ക്രീൻഷോട്ട്");
+define("LAN_FILE", "ഫയൽ");
+define("LAN_YOUTUBE_VIDEO", "യൂട്യൂബ് വീഡിയോ");
+define("LAN_YOUTUBE_PLAYLIST", "Youtube പ്ലേലിസ്റ്റ്");
+define("LAN_FILETYPES", "ഫയൽ തരങ്ങൾ");
+define("LAN_FILE_NOT_FOUND", "ഫയൽ കാണുന്നില്ല");
+define("LAN_FILES", "ഫയലുകൾ");
+define("LAN_SIZE", "വലുപ്പം");
+define("LAN_VERSION", "പതിപ്പ്");
+define("LAN_DOWNLOAD", "ഡൗൺലോഡ്");
+define("LAN_WEBSITE", "വെബ്സൈറ്റ്");
+define("LAN_COMMENTS", "അഭിപ്രായങ്ങൾ");
+define("LAN_LOCATION", "സ്ഥാനം");
+define("LAN_NO_RECORDS_FOUND", "റെക്കോർഡുകൾ ഒന്നും കണ്ടെത്തിയില്ല");
+define("LAN_RATING", "റേറ്റിംഗ്");
+define("LAN_IMAGE", "ചിത്രം");
+define("LAN_ABOUT", "സംബന്ധിച്ച്");
+define("LAN_TITLE", "ശീർഷകം");
+define("LAN_MESSAGE", "സന്ദേശം");
+define("LAN_USER", "ഉപയോക്താവ്");
+define("LAN_EMAIL", "ഈ - മെയില് വിലാസം");
+define("LAN_WROTE", "എഴുതി");
+define("LAN_RE_ORDER", "പുനഃക്രമീകരിക്കുക");
+define("LAN_RELATED", "അനുബന്ധമായ");
+define("LAN_CLOSE", "അടയ്ക്കുക");
+define("LAN_EXPAND", "വിടര്‍ത്തുക");
+define("LAN_ENTER_USRNAME_EMAIL", "നിങ്ങളുടെ ഉപയോക്തൃനാമം അല്ലെങ്കിൽ ഇമെയിൽ നൽകുക");
+define("LAN_PWD_REQUIRED", "പാസ്വേഡ് ആവശ്യമാണ്");
+define("LAN_SHOW", "കാണിക്കുക");
+define("LAN_GENERATE", "ഉണ്ടാക്കുക");
+define("LAN_SUMMARY", "സംഗ്രഹം");
+define("LAN_REQUIRED_BLANK", "ആവശ്യമായ ഫീൽഡ് (കൾ) ശൂന്യമായി അവശേഷിപ്പിച്ചിരിക്കുന്നു");
+define("LAN_PLEASEWAIT", "ദയവായി കാത്തിരിക്കുക");
+define("LAN_CHOOSE_FILE", "ഒരു ഫയൽ തിരഞ്ഞെടുക്കുക");
+
+
+?>
