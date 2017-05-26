811b105e13d1a54b2a94d621e5375606f7076fd6
Arun S. Sekher <arunshekher@users.noreply.github.com>
2017-05-26 12:06:01 +0400

    Add e107_languages/Malayalam/lan_membersonly.php

---
+++ e107_languages/Malayalam/lan_membersonly.php
@@ -0,0 +1,21 @@
+<?php
+/*
++---------------------------------------------------------------+
+|        e107 website content management system Malayalam Language File
+|        Released under the terms and conditions of the
+|        GNU General Public License (http://gnu.org).
+|        Last Modified: 2017/05/26 08:04:57
+|
+|        $Author: Arun $
++---------------------------------------------------------------+
+*/
+
+define("PAGE_NAME", "അംഗങ്ങൾ മാത്രം");
+define("LAN_MEMBERS_0", "നിയന്ത്രിത പ്രദേശം");
+define("LAN_MEMBERS_1", "ഇതൊരു നിയന്ത്രിത പ്രദേശമാണ്.");
+define("LAN_MEMBERS_2", "പ്രവേശനത്തിനായി ദയവായി [ലോഗിൻ ചെയ്യുക]");
+define("LAN_MEMBERS_3", "അല്ലെങ്കിൽ ഒരു അംഗമായി [രജിസ്റ്റർ] ചെയ്യുക.");
+define("LAN_MEMBERS_4", "മുൻപേജിലേക്ക് മടങ്ങാൻ ഇവിടെ ക്ലിക്കുചെയ്യുക.");
+
+
+?>
