a89513004a0c2fcba66e2cdac85dcf569f557bf7
Arun S. Sekher <arunshekher@users.noreply.github.com>
2017-05-26 06:54:18 +0400

    Add e107_languages/Malayalam/lan_email.php

---
+++ e107_languages/Malayalam/lan_email.php
@@ -0,0 +1,37 @@
+<?php
+/*
++---------------------------------------------------------------+
+|        e107 website content management system Malayalam Language File
+|        Released under the terms and conditions of the
+|        GNU General Public License (http://gnu.org).
+|        Last Modified: 2017/05/26 02:53:01
+|
+|        $Author: Arun $
++---------------------------------------------------------------+
+*/
+
+define("LAN_EMAIL_1", "നിന്ന്:");
+define("LAN_EMAIL_2", "അയച്ചയാളുടെ IP വിലാസം:");
+define("LAN_EMAIL_3", "ഇമെയിൽ ചെയ്ത ഇനം ഇതിൽ നിന്ന്‌");
+define("LAN_EMAIL_4", "ഇമെയിൽ അയയ്ക്കുക");
+define("LAN_EMAIL_5", "ഒരു സുഹൃത്തിന് ഈ ഐറ്റം ഇമെയിൽ ചെയ്യുക");
+define("LAN_EMAIL_6", "ഈ ഐറ്റത്തിൽ നിങ്ങൾക്ക് താൽപര്യമുണ്ടായിരിക്കാമെന്ന് ഞാൻ വിചാരിച്ചു ഇവിടെ നിന്ന് ");
+define("LAN_EMAIL_7", "ആർക്കെങ്കിലും ഇമെയിൽ ചെയ്യുക");
+define("LAN_EMAIL_8", "അഭിപ്രായം");
+define("LAN_EMAIL_9", "ക്ഷമിക്കണം - ഇമെയിൽ അയയ്ക്കാനാവില്ല");
+define("LAN_EMAIL_10", "മെയിൽ ഇതിലേക്ക് അയച്ചു");
+define("LAN_EMAIL_11", "ഇമെയിൽ അയച്ചു");
+define("LAN_EMAIL_13", "ഒരു സുഹൃത്തിന് ലേഖനം ഇമെയിൽ ചെയ്യുക");
+define("LAN_EMAIL_14", "ഒരു സുഹൃത്തിന് വാർത്താ_ഇനം ഇമെയിൽ ചെയ്യുക ");
+define("LAN_EMAIL_15", "ഉപയോക്തൃനാമം:");
+define("LAN_EMAIL_106", "അത് ഒരു സാധുവായ ഇമെയിൽ വിലാസമായി വെളിപ്പെടുന്നില്ല");
+define("LAN_EMAIL_185", "ലേഖനം അയയ്ക്കുക");
+define("LAN_EMAIL_186", "വാർത്താ ഇനം അയയ്ക്കുക");
+define("LAN_EMAIL_187", "അയയ്ക്കാനുള്ള ഇമെയിൽ വിലാസം");
+define("LAN_EMAIL_188", "ഈ വാർത്തയിൽ നിങ്ങൾക്ക് താല്പര്യമുണ്ടെന്ന് ഞാൻ കരുതി ഇവിടെ നിന്ന്");
+define("LAN_EMAIL_189", "ഈ ലേഖനത്തിൽ നിങ്ങൾക്ക് താല്പര്യമുണ്ടായിരിക്കാമെന്ന് ഞാൻ കരുതി");
+define("LAN_EMAIL_190", "ദൃശ്യമായ കോഡ് നൽകുക");
+define("LAN_SOCIAL_LINK_CHK", "ഈ ലിങ്ക് നോക്കുക:");
+
+
+?>
