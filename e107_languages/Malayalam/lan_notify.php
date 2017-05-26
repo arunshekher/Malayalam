345ac650df3af7d86dc1771322788d800ea0821f
Arun S. Sekher <arunshekher@users.noreply.github.com>
2017-05-26 18:18:00 +0400

    Add e107_languages/Malayalam/lan_notify.php

---
+++ e107_languages/Malayalam/lan_notify.php
@@ -0,0 +1,29 @@
+<?php
+/*
++---------------------------------------------------------------+
+|        e107 website content management system Malayalam Language File
+|        Released under the terms and conditions of the
+|        GNU General Public License (http://gnu.org).
+|        Last Modified: 2017/05/26 09:40:20
+|
+|        $Author: Arun $
++---------------------------------------------------------------+
+*/
+define("NT_LAN_US_1", "ഉപയോക്തൃ സൈൻഅപ്പ്");
+define("NT_LAN_UV_1", "ഉപയോക്തൃ സൈൻഅപ്പ് ദൃഢീകരിക്കപ്പെട്ടു");
+define("NT_LAN_UV_2", "യൂസർ ഐഡി:");
+define("NT_LAN_UV_3", "ഉപയോക്തൃ ലോഗിൻ നാമം:");
+define("NT_LAN_UV_4", "ഉപയോക്തൃ ഐ.പി.:");
+define("NT_LAN_LI_1", "ഉപയോക്താവ് പ്രവേശിച്ചു");
+define("NT_LAN_LO_1", "ഉപയോക്താവ് ലോഗ് ഔട്ട് ചെയ്തു");
+define("NT_LAN_LO_2", "സൈറ്റിൽ നിന്ന് പുറത്ത് പോയി");
+define("NT_LAN_FL_1", "ഫ്ലഡ് നിരോധനം");
+define("NT_LAN_FL_2", "ഐ.പി. വിലാസം ഫ്ലഡിങ് കാരണം നിരോധിച്ചു");
+define("NT_LAN_SN_1", "വാർത്താ ഇനം സമർപ്പിച്ചു");
+define("NT_LAN_ML_1", "ബൾക്ക് ഇമെയിൽ അയയ്ക്കുന്നത് പൂർത്തിയായി");
+define("NT_LAN_NU_1", "അപ്ഡേറ്റുചെയ്തു");
+define("NT_LAN_ND_1", "വാർത്ത ഇനം നീക്കം ചെയ്തു");
+define("NT_LAN_ND_2", "നീക്കം ചെയ്ത വാർത്താ ഇനത്തിന്റെ ഐഡി");
+
+
+?>
