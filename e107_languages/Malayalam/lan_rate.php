5b679b74140c6f388a9d95726930d9c080697419
Arun S. Sekher <arunshekher@users.noreply.github.com>
2017-05-26 18:26:25 +0400

    Add e107_languages/Malayalam/lan_rate.php

---
+++ e107_languages/Malayalam/lan_rate.php
@@ -0,0 +1,32 @@
+<?php
+/*
++---------------------------------------------------------------+
+|        e107 website content management system Malayalam Language File
+|        Released under the terms and conditions of the
+|        GNU General Public License (http://gnu.org).
+|        Last Modified: 2017/05/26 14:25:18
+|
+|        $Author: Arun $
++---------------------------------------------------------------+
+*/
+
+define("RATELAN_0", "വോട്ടുചെയ്യുക");
+define("RATELAN_1", "വോട്ടുകൾ");
+define("RATELAN_2", "ഈ ഇനം നിങ്ങൾ എങ്ങനെ റേറ്റ് ചെയ്യുന്നു?");
+define("RATELAN_3", "വോട്ടുചെയ്തതിന് നന്ദി!");
+define("RATELAN_4", "റേറ്റുചെയ്തിട്ടില്ല");
+define("RATELAN_5", "ഇത് റേറ്റുചെയ്യുക:");
+define("RATELAN_6", "ഇത് റേറ്റുചെയ്യാൻ ദയവായി ലോഗിൻ ചെയ്യൂ.");
+define("RATELAN_7", "ഇഷ്‌ടം");
+define("RATELAN_8", "അനിഷ്‌ടം");
+define("RATELAN_9", "നിങ്ങൾ ഇതിനകം വോട്ടുചെയ്തു");
+define("RATELAN_10", "റേറ്റിംഗ് ഇനങ്ങളൊന്നും ഐഡി ഇല്ല");
+define("RATELAN_11", "റേറ്റിംഗ് പരാജയപ്പെട്ടു");
+define("RATELAN_POOR", "മോശം");
+define("RATELAN_FAIR", "ന്യായമായത്");
+define("RATELAN_GOOD", "നല്ലത്");
+define("RATELAN_VERYGOOD", "വളരെ നല്ലത്");
+define("RATELAN_EXCELLENT", "ഉല്‍കൃഷ്‌ടമായത്");
+
+
+?>
