6256fef15945cbe6f27107336037ff9b0275d4f7
Arun S. Sekher <arunshekher@users.noreply.github.com>
2017-05-26 08:41:00 +0400

    Add e107_languages/Malayalam/lan_form_handler.php

---
+++ e107_languages/Malayalam/lan_form_handler.php
@@ -0,0 +1,31 @@
+<?php
+/*
++---------------------------------------------------------------+
+|        e107 website content management system Malayalam Language File
+|        Released under the terms and conditions of the
+|        GNU General Public License (http://gnu.org).
+|        Last Modified: 2017/05/26 04:34:32
+|
+|        $Author: Arun $
++---------------------------------------------------------------+
+*/
+
+define("LAN_EFORM_001", "അത് മാറ്റാൻ അവതാറിൽ ക്ലിക്കുചെയ്യുക");
+define("LAN_EFORM_002", "അവതാർ തിരഞ്ഞെടുക്കുക");
+define("LAN_EFORM_003", "അഥവാ");
+define("LAN_EFORM_004", "ഈ അവതാർ തിരഞ്ഞെടുക്കുക");
+define("LAN_EFORM_005", "ഒരു അവതാറുകളും ലഭ്യമല്ല");
+define("LAN_EFORM_006", "അഡ്മിൻ-ഒൺലി അറിയിപ്പ്: [br] [b] ഫോൾഡർ [x] [/ b] ശൂന്യമാണ്. [Br] ഉപയോക്താവിന് തിരഞ്ഞെടുക്കാനായി ഈ ഫോൾഡറിലേക്ക് ചില ഡീഫോൾട് അവതാർ ചിത്രങ്ങൾ അപ്ലോഡുചെയ്യുക.");
+define("LAN_EFORM_007", "മീഡിയ മാനേജർ");
+define("LAN_EFORM_008", "പ്രദർശിപ്പിക്കാനുള്ള നിരകൾ തിരഞ്ഞെടുക്കുക");
+define("LAN_EFORM_009", "നിരകൾ പ്രദർശിപ്പിക്കുക");
+define("LAN_EFORM_010", "ദ്രുത കാഴ്ച");
+define("LAN_EFORM_011", "ഉപയോക്തൃ പ്രൊഫൈലിലേക്ക് പോകുക");
+define("LAN_EFORM_012", "വിവിധ ഭാഷാ ഫീൽഡുകൾ");
+define("LAN_EFORM_013", "പട്ടികയിലേക്ക് പോകുക");
+define("LAN_EFORM_014", "മറ്റൊന്ന് സൃഷ്ടിക്കുക");
+define("LAN_EFORM_015", "നിലവിലുള്ളത് എഡിറ്റുചെയ്യുക");
+define("LAN_EFORM_016", "സമർപ്പിച്ചതിനുശേഷം:");
+
+
+?>
