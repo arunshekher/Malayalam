1df8d8079d9dba9a2cd316fe28814ebecc69d8ff
Arun S. Sekher <arunshekher@users.noreply.github.com>
2017-05-26 18:02:59 +0400

    Add e107_languages/Malayalam/lan_mail_handler.php

---
+++ e107_languages/Malayalam/lan_mail_handler.php
@@ -0,0 +1,22 @@
+<?php
+/*
++---------------------------------------------------------------+
+|        e107 website content management system Malayalam Language File
+|        Released under the terms and conditions of the
+|        GNU General Public License (http://gnu.org).
+|        Last Modified: 2017/05/26 14:01:15
+|
+|        $Author: Arun $
++---------------------------------------------------------------+
+*/
+
+define("LANMAILH_1", "നിർമ്മിച്ചത് e107 വെബ്സൈറ്റ് സിസ്റ്റം");
+define("LANMAILH_2", "ഇത് MIME ഫോർമാറ്റിലുള്ള ഒരു മൾട്ടി പാർട്ട് സന്ദേശം ആണ്.");
+define("LANMAILH_3", "ശരിയായി ഫോർമാറ്റുചെയ്തിട്ടില്ല");
+define("LANMAILH_4", "സെർവർ വിലാസം നിരസിച്ചു");
+define("LANMAILH_5", "സെർവറിൽ നിന്ന് പ്രതികരണമൊന്നുമില്ല");
+define("LANMAILH_6", "ഇ-മെയിൽ സെർവർ കണ്ടെത്താനായില്ല.");
+define("LANMAILH_7", "സാധുതയുള്ളതായി തോന്നുന്നു.");
+
+
+?>
