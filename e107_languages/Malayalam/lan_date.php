2fb510be947fd585f277fd47957df34b7b925798
Arun S. Sekher <arunshekher@users.noreply.github.com>
2017-05-26 06:29:57 +0400

    Add e107_languages/Malayalam/lan_date.php

---
+++ e107_languages/Malayalam/lan_date.php
@@ -0,0 +1,35 @@
+<?php
+/*
++---------------------------------------------------------------+
+|        e107 website content management system Malayalam Language File
+|        Released under the terms and conditions of the
+|        GNU General Public License (http://gnu.org).
+|        Last Modified: 2017/05/26 02:28:24
+|
+|        $Author: Arun $
++---------------------------------------------------------------+
+*/
+
+define("LANDT_01", "വർഷം");
+define("LANDT_02", "മാസം");
+define("LANDT_03", "ആഴ്ച");
+define("LANDT_04", "ദിവസം");
+define("LANDT_05", "മണിക്കൂർ");
+define("LANDT_06", "മിനിറ്റ്");
+define("LANDT_07", "സെക്കന്റ്");
+define("LANDT_01s", "വർഷങ്ങൾ");
+define("LANDT_02s", "മാസങ്ങൾ");
+define("LANDT_03s", "ആഴ്ചകൾ");
+define("LANDT_04s", "ദിവസങ്ങൾ");
+define("LANDT_05s", "മണിക്കൂറുകൾ");
+define("LANDT_06s", "മിനിറ്റുകൾ");
+define("LANDT_07s", "സെക്കൻഡുകൾ");
+define("LANDT_08", "മിനിറ്റ്");
+define("LANDT_08s", "മിനിറ്റുകൾ");
+define("LANDT_09", "സെക്കന്റ്");
+define("LANDT_09s", "സെക്കൻഡുകൾ");
+define("LANDT_AGO", "മുന്പ്");
+define("LANDT_10", "ഇപ്പോൾ");
+
+
+?>
